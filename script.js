ef67dd36bb32602f34dc83f893fb1f8fc7aee8c8
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-30 09:32:08 -0800

    Added Alert in html & script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Looks comfy don't it?";
 document.querySelector("body").appendChild(h2);
+function myFunction() {
+  alert("It's Too Hot Wait!");
+}
