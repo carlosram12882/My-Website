710f7abd15663538bea1b083eff49bb5c2cb0aed
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-30 09:26:00 -0800

    Changed Texts

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Looks comfy don't it?";
 document.querySelector("body").appendChild(h2);
