ce17dddb2b9dfa7b5b2e39b350f53c5d0b1631f8
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-30 09:53:46 -0800

    Changed button text and image

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const h2 = document.createElement("h2");
 h2.textContent = "Looks comfy don't it?";
 document.querySelector("body").appendChild(h2);
 function myFunction() {
-  alert("It's Too Hot Wait!");
+  alert("It's Too Hot Wait, FOR YOU!");
 }
